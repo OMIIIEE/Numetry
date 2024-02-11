1c8aaf12244ef46dc6281f547099cd7b2460477f
Om Prakash <117626285+OMIIIEE@users.noreply.github.com>
2024-02-11 20:18:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// App.js
+
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+import Header from './Header'
+import './App.css'
+import Navbar from './Navbar';
+import MainBody from './MainBody';
+import BottomPart from './BottomPart';
+import AboutUs from './NavCom/AboutUs/AboutUs';
+
+
+const App = () => {
+  return (
+    <Router>
+    <div>
+     <Header/>
+      {/* Other components and content */}
+      <Navbar/>
+        {/* <AboutUs/> */}
+      <MainBody/>
+      <BottomPart/>
+      
+    </div>
+    </Router>
+  );
+}
+
+export default App;
